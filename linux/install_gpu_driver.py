82f76da0a83b3b98480189986cd8a95cde924e3d
Maciej Strzelczyk <strzelczyk@google.com>
2023-08-01 17:17:05 +0200

    Fixing installation for Ubuntu 22

--- linux/install_gpu_driver.py
+++ linux/install_gpu_driver.py
@@ -293,6 +293,8 @@ def install_dependencies_debian_ubuntu(system: System, version: str) -> bool:
     run("apt update")
     run(f"apt install -y linux-headers-{kernel_version} "
         "software-properties-common pciutils gcc make dkms")
+    if system == System.Ubuntu and version.startswith("22"):
+        run("update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 100 --slave /usr/bin/g++ g++ /usr/bin/g++-12 --slave /usr/bin/gcov gcov /usr/bin/gcov-12")
     return False
 
 
