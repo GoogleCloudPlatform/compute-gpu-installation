35b461718a225d78eec5957c7b963fda744570ec
Maciej Strzelczyk <strzelczyk@google.com>
2025-04-04 15:35:03 +0200

    Updating startup script download location

--- linux/startup_script.sh
+++ linux/startup_script.sh
@@ -7,5 +7,5 @@ fi
 mkdir -p /opt/google/cuda-installer
 cd /opt/google/cuda-installer/ || exit
 
-curl -fSsL -O https://github.com/GoogleCloudPlatform/compute-gpu-installation/releases/download/cuda-installer-v1.2.0/cuda_installer.pyz
+curl -fSsL -O https://storage.googleapis.com/compute-gpu-installation-us/installer/v1.3.0/cuda_installer.pyz
 python3 cuda_installer.pyz install_cuda
