5e08696d55f09733c665a6624ba28ad4f98714eb
Maciej Strzelczyk <strzelczyk@google.com>
2025-04-09 17:32:16 +0200

    Use latest version of the script all the time

--- linux/startup_script.sh
+++ linux/startup_script.sh
@@ -7,5 +7,5 @@ fi
 mkdir -p /opt/google/cuda-installer
 cd /opt/google/cuda-installer/ || exit
 
-curl -fSsL -O https://storage.googleapis.com/compute-gpu-installation-us/installer/v1.3.0/cuda_installer.pyz
+curl -fSsL -O https://storage.googleapis.com/compute-gpu-installation-us/installer/latest/cuda_installer.pyz
 python3 cuda_installer.pyz install_cuda
